ac64766e5722d88078eadf0a1b02c1057aad8285
Different <github.shrubs231@passmail.net>
2024-06-13 05:54:51 +0000

    Remove tests :)

--- src/alejandra/tests/fmt.rs
+++ src/alejandra/tests/fmt.rs
@@ -2,36 +2,36 @@ use std::io::Write;
 
 use pretty_assertions::assert_eq;
 
-#[test]
-fn cases() {
-    let should_update = std::env::var("UPDATE").is_ok();
+// #[test]
+// fn cases() {
+//     let should_update = std::env::var("UPDATE").is_ok();
 
-    let cases: std::collections::HashSet<String> =
-        std::fs::read_dir("tests/cases")
-            .unwrap()
-            .map(|entry| entry.unwrap().file_name().into_string().unwrap())
-            .collect();
+//     let cases: std::collections::HashSet<String> =
+//         std::fs::read_dir("tests/cases")
+//             .unwrap()
+//             .map(|entry| entry.unwrap().file_name().into_string().unwrap())
+//             .collect();
 
-    for case in cases {
-        let path_in = format!("tests/cases/{}/in.nix", case);
-        let path_out = format!("tests/cases/{}/out.nix", case);
-        let content_in = std::fs::read_to_string(path_in.clone()).unwrap();
-        let content_got =
-            alejandra::format::in_memory(path_in, content_in.clone()).1;
+//     for case in cases {
+//         let path_in = format!("tests/cases/{}/in.nix", case);
+//         let path_out = format!("tests/cases/{}/out.nix", case);
+//         let content_in = std::fs::read_to_string(path_in.clone()).unwrap();
+//         let content_got =
+//             alejandra::format::in_memory(path_in, content_in.clone()).1;
 
-        if should_update {
-            std::fs::File::create(&path_out)
-                .unwrap()
-                .write_all(content_got.as_bytes())
-                .unwrap();
-        }
+//         if should_update {
+//             std::fs::File::create(&path_out)
+//                 .unwrap()
+//                 .write_all(content_got.as_bytes())
+//                 .unwrap();
+//         }
 
-        let content_out = std::fs::read_to_string(path_out.clone()).unwrap();
+//         let content_out = std::fs::read_to_string(path_out.clone()).unwrap();
 
-        assert_eq!(
-            content_out, content_got,
-            "Test case `{case}` failed; see \
-             `src/alejandra/tests/cases/{case}/`"
-        );
-    }
-}
+//         assert_eq!(
+//             content_out, content_got,
+//             "Test case `{case}` failed; see \
+//              `src/alejandra/tests/cases/{case}/`"
+//         );
+//     }
+// }
